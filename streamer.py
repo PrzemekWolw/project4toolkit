00b4f16ca7f6cff36aa1e7c0d615c060cae70b72
defaultusername001 <122072742+defaultusername001@users.noreply.github.com>
2023-01-05 23:41:50 -0500

    Update streamer.py

--- streamer.py
+++ streamer.py
@@ -11,23 +11,23 @@ for line in lines:
     values = line.split()
 
     # Extract the position and rotation values
-    pos_x = float(values[0])
-    pos_y = float(values[1])
-    pos_z = float(values[2])
-    rot_x = float(values[3])
-    rot_y = float(values[4])
-    rot_z = float(values[5])
+    pos_x = float(values[0].replace(",", ""))
+    pos_y = float(values[1].replace(",", ""))
+    pos_z = float(values[2].replace(",", ""))
+    rot_x = float(values[3].replace(",", ""))
+    rot_y = float(values[4].replace(",", ""))
+    rot_z = float(values[5].replace(",", ""))
 
     # Extract the name of the .dae file
-    dae_name = (values[7])
+    dae_name = (values[7].replace(",", ""))
 
     # Check if the .dae file exists
-    if not os.path.exists(os.path.join("/path/to/folder/", dae_name)):
+    if not os.path.exists(os.path.join("C:/Users/user/Downloads/P4Toolkit/brook", dae_name + ".dae")):
         print(".dae file not found:", dae_name)
         continue
 
     # Import the .dae file
-    bpy.ops.wm.collada_import(filepath=os.path.join("/path/to/folder/", dae_name))
+    bpy.ops.wm.collada_import(filepath=os.path.join("C:/Users/user/Downloads/P4Toolkit/brook", dae_name + ".dae"))
 
     # Select the imported object
     obj = bpy.context.selected_objects[0]
@@ -35,3 +35,12 @@ for line in lines:
     # Set the position and rotation of the object
     obj.location = (pos_x, pos_y, pos_z)
     obj.rotation_euler = (rot_x, rot_y, rot_z)
+    
+     # Set the export path for the modified .dae file
+    export_path = os.path.join("C:/Users/user/Downloads/P4Toolkit/brook/streamer", dae_name + ".dae")
+        
+        # Export the object as a .dae file
+    bpy.ops.wm.collada_export(filepath=export_path)
+        
+        # Remove the object from the scene
+    bpy.ops.object.delete()
