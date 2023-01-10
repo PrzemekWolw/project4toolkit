02f274413531c8c400384f826281869a8ea294eb
defaultusername001 <122072742+defaultusername001@users.noreply.github.com>
2023-01-10 11:11:03 -0500

    fixes

--- combined/P4Toolkit (2).py
+++ combined/P4Toolkit (2).py
@@ -1,6 +1,7 @@
 def script1_function():
-    print("Checking WDRs")
+    print("Running script 1 function")
 
+print("Running script 1")
 script1_function()
 
 import os
@@ -9,7 +10,10 @@ import sys
 import bpy
 import mathutils
 
+# Select all objects in the scene
 bpy.ops.object.select_all(action='SELECT')
+
+# Delete all selected objects
 bpy.ops.object.delete()
 
 
@@ -92,7 +96,7 @@ class Mesh:
     def generate(self) -> None:
         vert_index = self.vertex_offset
 
-        
+        # print(str(self.index) + " - " + str(len(self.vert_data)))
         for raw_vert in self.vert_data:
             coord = raw_vert.split(" / ")[0].split(" ")
             for i in range(len(coord)):
@@ -107,7 +111,8 @@ class Mesh:
                 uv = raw_vert.split(" / ")[4].split(" ")
             uv[0] = float(uv[0])
             uv[1] = -float(uv[1])
-           
+            # for i in range(len(uv)):
+            #     uv[i] = float(uv[i])
             
             vertex = Vertex(vert_index, coord, normal, uv)
             self.vertices.append(vertex)
@@ -153,6 +158,13 @@ class Material:
 
         mtl = ""
         mtl += f"newmtl {self.name}" + "\n"
+        # mtl += "Ka " + " ".join([str(o) for o in self.Ka]) + "\n"
+        # mtl += "Kd " + " ".join([str(o) for o in self.Kd]) + "\n"
+        # mtl += "Ks " + " ".join([str(o) for o in self.Ks]) + "\n"
+        # mtl += "Ns " + str(self.Ns) + "\n"
+        # mtl += "Ni " + str(self.Ni) + "\n"
+        # mtl += "d " + str(self.d) + "\n"
+        # mtl += "illum " + str(self.illum) + "\n"
         if len(self.map_Kd) > 0:
             mtl += f"map_Kd {self.map_Kd}" + "\n"
         if len(self.map_bump) > 0:
@@ -172,7 +184,7 @@ class MaterialParser:
 
         for index, shader in enumerate(self.shaders):
             material = Material(f"MAT_{str(index)}")
-            material.map_Kd = shader
+            material.map_Kd = shader # TODO make something better
             materials.append(material)
         return materials
 
@@ -207,8 +219,9 @@ class MeshParser:
                 if mesh is not None:
                     self.meshes.append(mesh)
 
-                
+                # mesh_index = int(re.sub(r"\D", "", line))
                 mesh_index += 1
+                # print(mesh_index)
                 mesh = Mesh(mesh_index)
                 try:
                     mesh.material = self.materials[mesh_index]
@@ -282,6 +295,13 @@ class MeshParser:
             material_count += 1 if mesh.material is not None else 0
 
         mtl = "".join(material.generate() for material in self.materials)
+        print("--------------------")
+        print(f"Mesh count: {len(self.meshes)}")
+        print(f"Vertex count: {vert_count}")
+        print(f"Face count: {face_count}")
+        print(f"Material count: {material_count}")
+        print("--------------------")
+
         return {"obj": obj, "mtl": mtl}
 
 
@@ -292,6 +312,7 @@ def parse_odr(lines):
     shadinggroups = []
     skeletons = ""
 
+    # Shader groups
     regex_shadinggroup = r"(s|S)hadinggroup(\n|\s|){\n\t{0,1}(s|S)haders \d{1,999}\n\t{0,1}{\n\t{2}[^}]+}\n}"
 
     if match_shadinggroup := re.search(regex_shadinggroup, lines):
@@ -340,9 +361,12 @@ def parse_odr(lines):
                     and "{" not in line
                     and "DiffuseSampler" in line
                 ):
-                    sps_data.append(re.sub(r"\\", "/", re.sub(line.split(" ")[0], "", line).lstrip()).replace(".otx", ".png"))
+                    sps_data.append(re.sub(r"\\", "/", re.sub(line.split(" ")[0], "", line).lstrip()).replace(".otx", ".png")) # TODO make something better
             previous_line = line
         shadinggroups = sps_data
+    # Shader groups
+
+    # Skeletons
     regex_skeletons = r"(s|S)kel(eton|)((\n|\s|){\n\t{0,1}[\w\.\s\t\\\-]+\n}|[ \w\\\.\d\-]+)"
 
     if match_skeletons := re.search(regex_skeletons, lines):
@@ -358,6 +382,10 @@ def parse_odr(lines):
         if skel.lower().endswith(".skel")
     ]
     skeletons = new_skeletons
+    # Skeletons
+
+    # LOD groups
+
     depth = 0
     in_lodgroup = -1
     lodgroup_lines = []
@@ -400,6 +428,7 @@ def parse_odr(lines):
             if len(lod_value) > 0:
                 odr_data["lodgroup"][lod_type] = lod_value
 
+    # LOD groups
 
     if shadinggroups:
         odr_data["shaders"] = shadinggroups
@@ -413,7 +442,7 @@ if __name__ == "__main__" and len(sys.argv) != 2:
     print("Invalid arguments")
     exit(0)
 
-
+# Code to recursively process all .odr files in the current directory and subdirectories
 for root, dirs, files in os.walk("."):
     for file in files:
         if file.endswith(".odr"):
@@ -422,6 +451,7 @@ for root, dirs, files in os.walk("."):
             with open(input_file_path, "r") as f:
                 raw_odr_lines = f.read()
                 odr_data = parse_odr(raw_odr_lines)
+                # rest of the code to process the .odr file goes here
 
             for lodgroup in odr_data["lodgroup"]:
                 output_obj_name = f"{input_name}.obj"
@@ -450,8 +480,14 @@ for root, dirs, files in os.walk("."):
 import bpy
 import os
 
+# Select all objects in the scene
 bpy.ops.object.select_all(action='SELECT')
+
+# Delete all selected objects
 bpy.ops.object.delete()
+
+# Set the active rendering engine to 'BLANK'
+
 
 def find_base_color_node(mat):
     try:
@@ -470,22 +506,32 @@ def get_node_name(node):
     except Exception:
         return None
 
+# Set the path to the folder containing the .obj files here
 path = os.getcwd()
+
+# Iterate over all .obj files in the folder
 for obj_file in os.listdir(path):
     if obj_file.endswith(".obj"):
+        # Import the .obj file
         bpy.ops.import_scene.obj(filepath=os.path.join(path, obj_file))
+
+        # Get all materials with names starting with "Standardmaterial"
         mat_names = [
             mat.name for mat in bpy.data.materials
             if mat.name.startswith("MAT")
         ]
 
+        # Rename the materials
         for mat_name in mat_names:
             mat = bpy.data.materials[mat_name]
             node = find_base_color_node(mat)
             if new_name := get_node_name(node):
                 mat.name = new_name
-                
+
+        # Export the .obj file with the new material names
         bpy.ops.export_scene.obj(filepath=os.path.join(path, obj_file))
+
+        # Remove the object from the scene
         bpy.ops.object.delete()
 
 CONVERT_DIR = os.getcwd()
@@ -502,8 +548,14 @@ def file_iter(path, ext):
 import os
 import bpy
 
+# Select all objects in the scene
 bpy.ops.object.select_all(action='SELECT')
+
+# Delete all selected objects
 bpy.ops.object.delete()
+
+# Set the active rendering engine to 'BLANK'
+
 
 def reset_blend():
     bpy.ops.wm.read_factory_settings(use_empty=True)
@@ -526,6 +578,7 @@ if __name__ == "__main__":
     convert_recursive(CONVERT_DIR)
 
       
+
 def script2_function():
     print("Running script 2 function")
 
@@ -536,24 +589,30 @@ import bpy
 import os
 import mathutils
 
+# Select all objects in the scene
 bpy.ops.object.select_all(action='SELECT')
+
+# Delete all selected objects
 bpy.ops.object.delete()
 
+# Set the active rendering engine to 'BLANK'
 
 #this will convert the models position and rotation (w, x, y, z) quaternion by using the decrypted .wpl stream files. there is a known error here that in some fringe cases the w rotation is flipped positive when it should be negative, or vice versa. 
 
 #it is possible that RAGE is making a correction on its own here, possibly by using the last two values of the .wpl, but it is unknown. implement a fix for the known flipped w rotations.
 
 #the likely fix is that on LOD import, check if the W rotation matches on both files, if they do not match, find the one with the +W to a -W to match, RAGE is only making positive W errors
-
-
+      
+# Open the stream.txt file and read the lines
 with open("stream.txt", "r") as stream:
     lines = stream.readlines()
 
+# Iterate through each line in the file
 for line in lines:
+    # Split the line into a list of values
     values = line.split()
 
-    # Extract the position and rotation values from .wpl
+    # Extract the position and rotation values
     pos_x = float(values[0].replace(",", ""))
     pos_y = float(values[1].replace(",", ""))
     pos_z = float(values[2].replace(",", ""))
@@ -561,33 +620,52 @@ for line in lines:
     rot_y = float(values[4].replace(",", ""))
     rot_z = float(values[5].replace(",", ""))
     rot_w_str = values[6].replace(",", "")
-    rot_w = -float(rot_w_str[1:]) if rot_w_str[0] == "-" else float(rot_w_str)
+    if rot_w_str[0] == "-":
+        rot_w = -float(rot_w_str[1:])
+    else:
+        rot_w = float(rot_w_str)
+        
+    # Convert the rotational values to a quaternion
     quat = mathutils.Quaternion((rot_w, rot_x, rot_y, rot_z))
+    
+    # Extract the name of the .dae file
     dae_name = (values[7].replace(",", ""))
 
     cwd = os.getcwd()
-    filepath = os.path.join(cwd, f"{dae_name}.dae")
+    # Construct the file path to the .dae file
+    filepath = os.path.join(cwd, dae_name + ".dae")
+    # Check if the .dae file exists
     if not os.path.exists(filepath):
-        print(".dae file not found:", dae_name)
+        #print(".dae file not found:", dae_name)
         continue
 
-    bpy.ops.wm.collada_import(filepath=os.path.join(cwd, f"{dae_name}.dae"))
-
+    # Import the .dae file
+    bpy.ops.wm.collada_import(filepath = os.path.join(cwd, dae_name + ".dae"))
+    
+    
+    # Iterate over all the objects in the scene
     for obj in bpy.context.scene.objects:
+    # Set the rotation mode to "QUATERNION"
         obj.rotation_mode = "QUATERNION"
+    # Set the rotation to the quaternion value
         obj.rotation_quaternion = quat
         obj.location = (pos_x, pos_y, pos_z)
-
-    export_path = os.path.join(cwd, f"{dae_name}.dae")
-
+        
+    # Set the export path for the modified .dae file
+    export_path = os.path.join(cwd, dae_name + ".dae")
+        
+    # Export the object as a .dae file
     bpy.ops.wm.collada_export(filepath=export_path)
+        
+    # Remove the object from the scene
     bpy.ops.object.delete()
-
+    
     print(dae_name)
     
 def script3_function():
-    print("Cleaning Materials")
+    print("Running script 3 function")
 
+print("Running script 3")
 script3_function()
 
 
@@ -596,39 +674,56 @@ import bpy
 import os
 import re
 
-
+# Select all objects in the scene
 bpy.ops.object.select_all(action='SELECT')
+
+# Delete all selected objects
 bpy.ops.object.delete()
 
-
+# Get the current working directory (cwd)
 cwd = os.getcwd()
+
+# Iterate through all files in the cwd
 for file in os.listdir(cwd):
+    # Check if the file is a .dae file
     if file.endswith(".dae"):
+        # Import the .dae file
         bpy.ops.wm.collada_import(filepath=file)
 
-
+        # Apply the original function to each object in the .dae file
         for ob in bpy.context.selected_editable_objects:    
+            # Iterate over each material slot of the object
             for m_slot in ob.material_slots:
+                # Skip the material slot if it does not have a material
                 if not m_slot.material:
                     continue
+                # Get the material's name
                 s1 = m_slot.material.name
+                # Split the name by the "." character and keep only the first part
                 sArr = s1.split(".")
                 m_slot.material.name = sArr[0]
 
+        # Apply the modified function to each object in the .dae file
         for ob in bpy.context.selected_editable_objects:    
+            # Iterate over each material slot of the object
             for m_slot in ob.material_slots:
+                # Skip the material slot if it does not have a material
                 if not m_slot.material:
                     continue
+                # Get the material's name
                 s1 = m_slot.material.name
+                # Use a regular expression to replace any occurrence of ".\d{3}" with an empty string
                 s1 = re.sub(r"\.\d{3}", "", s1)
                 m_slot.material.name = s1
 
+        # Export and overwrite the old .dae file
         bpy.ops.wm.collada_export(filepath=file)
- 
+        
+        # Remove the object from the scene
         bpy.ops.object.delete()
         
 def script4_function():
-    print("Cleaning Materials")
+    print("Running script 4 function")
 
 print("Running script 4")
 script4_function()
@@ -637,50 +732,73 @@ import bpy
 import os
 import re
 
-
+# Select all objects in the scene
 bpy.ops.object.select_all(action='SELECT')
+
+# Delete all selected objects
 bpy.ops.object.delete()
 
-
+# Get the current working directory (cwd)
 cwd = os.getcwd()
 
+# Iterate through all files in the cwd
 for file in os.listdir(cwd):
+    # Check if the file is a .dae file
     if file.endswith(".dae"):
+        # Import the .dae file
         bpy.ops.wm.collada_import(filepath=file)
 
-
+        # Apply the original function to each object in the .dae file
         for ob in bpy.context.selected_editable_objects:    
+            # Iterate over each material slot of the object
             for m_slot in ob.material_slots:
+                # Skip the material slot if it does not have a material
                 if not m_slot.material:
                     continue
+                # Get the material's name
                 s1 = m_slot.material.name
+                # Split the name by the "." character and keep only the first part
                 sArr = s1.split(".")
                 m_slot.material.name = sArr[0]
 
+        # Apply the modified function to each object in the .dae file
         for ob in bpy.context.selected_editable_objects:    
+            # Iterate over each material slot of the object
             for m_slot in ob.material_slots:
+                # Skip the material slot if it does not have a material
                 if not m_slot.material:
                     continue
+                # Get the material's name
                 s1 = m_slot.material.name
+                # Use a regular expression to replace any occurrence of ".\d{3}" with an empty string
                 s1 = re.sub(r"\.\d{3}", "", s1)
                 m_slot.material.name = s1
 
+        # Export and overwrite the old .dae file
         bpy.ops.wm.collada_export(filepath=file)
         
+        # Remove the object from the scene
         bpy.ops.object.delete()
         
 def script5_function():
-    print("Moving Files")
+    print("Running script 5 function")
 
+print("Running script 5")
 script5_function()        
         
         
 import os
+# Set the root directory to the cwd
 root_directory = os.getcwd()
+# Iterate over all the files and subdirectories in the directory
 for root, dirs, files in os.walk(root_directory):
+    # Iterate over all the files in the current directory
     for filename in files:
+    # Check if the file is a .mtl or .obj file
         if filename.endswith('.mtl') or filename.endswith('.obj'):
+    # Construct the file path
             filepath = os.path.join(root, filename)
+        # Delete the file
             os.remove(filepath)
         
         
