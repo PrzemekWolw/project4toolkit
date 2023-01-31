813e03ed6f0ff7465e4c80f39602078172630d8e
defaultusername001 <122072742+defaultusername001@users.noreply.github.com>
2023-01-30 21:46:42 -0500

    randomizes the LOD texture for better variety

--- DEVTOOLS/gray.py
+++ DEVTOOLS/gray.py
@@ -1,9 +1,11 @@
 import bpy
 import os
+import random
 
 bpy.ops.object.select_all(action='SELECT')
 bpy.ops.object.delete()
 
+gray_names = ['gray', 'gray2', 'gray3', 'gray4', 'gray5', 'gray6']
 cwd = os.getcwd()
 for filename in os.listdir(cwd):
     if filename.endswith(".dae"):
@@ -13,7 +15,7 @@ for filename in os.listdir(cwd):
             if obj.type == 'MESH':
                 if obj.name.endswith("_a130") or obj.name.endswith("_a250"):
                     obj.data.materials.clear()
-                    gray = bpy.data.materials.new("gray")
+                    gray = bpy.data.materials.new(random.choice(gray_names))
                     obj.data.materials.append(gray)
         bpy.ops.wm.collada_export(filepath=file_path)
         bpy.ops.object.select_all(action='SELECT')
