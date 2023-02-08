79146467c43d3c9331466b2c2562e57fc6eafaeb
utopiadeferred <122072742+defaultusername001@users.noreply.github.com>
2023-02-07 19:43:17 -0500

    false positive fixes

--- DEVTOOLS/jsonmaker.py
+++ DEVTOOLS/jsonmaker.py
@@ -10,4 +10,4 @@ dae_files = [f for f in os.listdir(folder_path) if f.endswith('.dae')]
 with open('output.txt', 'w') as f:
     for dae_file in dae_files:
         # Write the line to the file, replacing "XXX" with the current .dae file's name
-        f.write(f'{"{"}"class":"TSStatic","persistentId":"","__parent":"NJ","position":[1,0,0],"collisionType":"Visible Mesh Final","decalType":"None","dynamic":true,"meshCulling":true,"prebuildCollisionData":true,"scale":[1,1,1],"shapeName":"/levels/smallgriddark/map/nj/{dae_file}","useInstanceRenderData":true {"}"}\n')
+        f.write(f'{"{"}"class":"TSStatic","persistentId":"","__parent":"MANHAT","position":[1,0,0],"collisionType":"Visible Mesh Final","decalType":"None","dynamic":true,"meshCulling":true,"prebuildCollisionData":false,"scale":[1,1,1],"shapeName":"/levels/smallgriddark/map/nj/{dae_file}","useInstanceRenderData":true {"}"}\n')
